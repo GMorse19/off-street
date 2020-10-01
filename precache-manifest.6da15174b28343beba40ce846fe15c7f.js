10bfbbf3f60ee86e168b4d3a7e2299a37c4082ca
GMorse19 <malorse@comcast.net>
2020-10-01 13:49:01 -0400

    Updates

---
+++ precache-manifest.6da15174b28343beba40ce846fe15c7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c398356e4f84bbf28700109722d879e2",
+    "url": "/off-street/index.html"
+  },
+  {
+    "revision": "b99fcc06087f5a43b148",
+    "url": "/off-street/static/css/main.23968b7b.chunk.css"
+  },
+  {
+    "revision": "ebfc530509ae88594ee0",
+    "url": "/off-street/static/js/2.d239b528.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/off-street/static/js/2.d239b528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99fcc06087f5a43b148",
+    "url": "/off-street/static/js/main.4b458278.chunk.js"
+  },
+  {
+    "revision": "bfdb22b4134780f5f141",
+    "url": "/off-street/static/js/runtime-main.4bc4cb71.js"
+  },
+  {
+    "revision": "906e95ca6c744b6c67914b18189b45ef",
+    "url": "/off-street/static/media/FlagCutOut2.906e95ca.png"
+  },
+  {
+    "revision": "7eefcff51db74fe9ee18c4f73c628b72",
+    "url": "/off-street/static/media/debris2.7eefcff5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/off-street/precache-manifest.005068c6ef31c6ebcaeccdb52e8c680e.js"
+  "/off-street/precache-manifest.6da15174b28343beba40ce846fe15c7f.js"
 );
 
 self.addEventListener('message', (event) => {
