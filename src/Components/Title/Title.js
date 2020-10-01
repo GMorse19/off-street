7956d23ed017a2bd830d8443617719f7d88a978b
GMorse19 <malorse@comcast.net>
2020-10-01 09:44:51 -0400

    Image helpers

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -4,13 +4,17 @@ import { Parallax } from 'react-scroll-parallax'
 
 import './Title.scss'
 
+import { logo } from '../../helpers/images/logosArray'
+
+// const logo = process.env.PUBLIC_URL + '/Logo/OSlogo.png';
+
 const Title = () => (
   <div>
   <Container className='logo-container'>
     <Row>
       <Col>
         <Parallax className="custom-class logo-div" y={[-80, 80]} tagOuter="figure">
-          <img className='home-logo' width='300px' src='./Logo/OSlogo.png' alt='rustic'/>
+          <img className='home-logo' width='300px' src={logo[1]} alt='rustic'/>
         </Parallax>
       </Col>
       <Col>
