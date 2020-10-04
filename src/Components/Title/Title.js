cc7c0f3c833a0897b31c0c03798079ab5dcf8cb1
GMorse19 <malorse@comcast.net>
2020-10-03 23:03:25 -0400

    Changed size of heading

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -8,25 +8,25 @@ import { logo } from '../../helpers/images/Logos/logosArray'
 
 const Title = (props) => (
   <div>
-  <Container className='logo-container'>
-    <Row>
-      <Col>
-        <Parallax className="custom-class logo-div" y={[-50, 50]} tagOuter="figure">
-          <img className='home-logo' width='300px' src={logo[1]} alt='rustic'/>
-        </Parallax>
-        <h1 className='page-heading'>{props.heading}</h1>
-      </Col>
+    <Container className='logo-container'>
+      <Row>
+        <Col>
+          <Parallax className="custom-class logo-div" y={[-50, 50]} tagOuter="figure">
+            <img className='home-logo' width='300px' src={logo[1]} alt='rustic'/>
+          </Parallax>
+          <h1 className='page-heading'>{props.heading}</h1>
+        </Col>
 
-      <Col>
-        <Parallax className="custom-class" y={[-50, 50]} tagOuter="figure">
-          <div className='title-div'>
-            <h1 className='title'>Off Street</h1>
-            <h5>Carpentry with a re-purpose</h5>
-          </div>
-        </Parallax>
-      </Col>
-    </Row>
-  </Container>
+        <Col>
+          <Parallax className="custom-class" y={[-50, 50]} tagOuter="figure">
+            <div className='title-div'>
+              <h1 className='title'>Off Street</h1>
+              <h5>Carpentry with a re-purpose</h5>
+            </div>
+          </Parallax>
+        </Col>
+      </Row>
+    </Container>
   </div>
 )
 
