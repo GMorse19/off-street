da715e6ee8f3d01042275bf660dec561074b7972
GMorse19 <malorse@comcast.net>
2020-10-05 22:06:55 -0400

    Added columns to menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import { fallDown as Menu } from 'react-burger-menu'
+import { Row, Col, Container } from 'react-bootstrap'
 
 import { icon } from '../../helpers/images/Icons/iconsArray'
+import LogoFloat from '../LogoFloat/LogoFloat'
 
 import './Header.scss'
 
@@ -35,11 +37,23 @@ class Header extends React.Component {
           onStateChange={(state) => this.handleChange(state)}
           width={ '100vw' }
         >
-          <a className='menu-link' onClick={() => this.closeMenu()} href='#/'>Home</a>
-          <a className='menu-link' onClick={() => this.closeMenu()} href='#about'>About</a>
-          <a className='menu-link' onClick={() => this.closeMenu()} href='#shop'>Shop</a>
-          <a className='menu-link' onClick={() => this.closeMenu()} href='#contact'>Contact</a>
-          <a className='menu-link' onClick={() => this.closeMenu()} href='#gallery'>Gallery</a>
+        <Container>
+          <Row style={{ paddingTop: '5vh' }}>
+            <Col lg={6}>
+              <LogoFloat
+                left={-30}
+                right={30}
+              />
+            </Col>
+            <Col lg={2}>
+              <a className='menu-link' onClick={() => this.closeMenu()} href='#/'>Home</a>
+              <a className='menu-link' onClick={() => this.closeMenu()} href='#about'>About</a>
+              <a className='menu-link' onClick={() => this.closeMenu()} href='#shop'>Shop</a>
+              <a className='menu-link' onClick={() => this.closeMenu()} href='#contact'>Contact</a>
+              <a className='menu-link' onClick={() => this.closeMenu()} href='#gallery'>Gallery</a>
+            </Col>
+          </Row>
+          </Container>
         </Menu>
         <button className='burger-menu'><img
           src={icon.button.burger}
