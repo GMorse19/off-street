98c6914d41875b16baba9da7c3815a54dcb58487
GMorse19 <malorse@comcast.net>
2020-10-13 22:26:41 -0400

    Responsive mobile

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends React.Component{
   }
 
   listenScrollEvent = e => {
-    if (window.scrollY > 1000 && window.scrollY <= 2400) {
+    if (window.scrollY > 1000 && window.scrollY <= 2500) {
       this.setState({background: 'img2 App'})
-    } else if (window.scrollY > 2400) {
+    } else if (window.scrollY > 2500) {
       this.setState({background: 'img3 App'})
     } else {
       this.setState({background: 'img1 App'})
