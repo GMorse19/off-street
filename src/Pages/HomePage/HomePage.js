e69257f110bb0dd4883d78c697a49067dcd4ea8f
GMorse19 <malorse@comcast.net>
2020-10-17 17:35:55 -0400

    Working on resizing window

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import SplashIntro from '../../Components/SplashIntro/SplashIntro'
+// import SplashIntro from '../../Components/SplashIntro/SplashIntro'
 import Connect from '../../Components/Connect/Connect'
 import Footer from '../../Components/Footer/Footer'
 import Title from '../../Components/Title/Title'
@@ -24,17 +24,34 @@ class HomePage extends React.Component {
         <Title
           tagline='"Revive, Repurpose, and Reinvent."'
         />
-        <SplashIntro />
+
         <InfoRow
+          header='Rescue, Repurpose, Reinvent'
+          sectionColor='#FFF'
+          headerFont='60px'
+          contentFont='25px'
+          content="Our mission at Off Street is to rescue the forgotten scraps and cutoffs from construction projects from their inevitable trip to the depths of a dumpster. Re-purpose these forlorn timbers and return a quality hand crafted item with it's own unique history. We like to think of it as a sort of retirement plan for forgotten wood"
+          buttonContent='SHOP'
+          href='#shop'
+          padding='2%'
+          backgroundUrl={stock.stock.standardFlag}
+        />
+
+        <InfoRow
+          y={[-15,10]}
           header='Handmade wood burned flags'
-          headerColor='#FFF'
+          sectionColor='#FFF'
           headerFont='60px'
           content='Browse through our extensive list of flags'
+          contentColor='#000'
           buttonContent='Flags'
           href='#shop'
+          padding='10%'
           backgroundUrl={stock.stock.workShop}
         />
+
         <BestSellers />
+
         <Connect />
         <Footer />
       </div>
