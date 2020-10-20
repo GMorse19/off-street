bc00a06f9fac69b68ee3134e40ac3eaf69147b41
GMorse19 <malorse@comcast.net>
2020-10-20 18:59:48 -0400

    Added resized images

--- src/helpers/images/Stock/stockArray.js
+++ src/helpers/images/Stock/stockArray.js
@@ -1,12 +1,15 @@
 export const stock = {
   stock: {
     workShop: process.env.PUBLIC_URL + '/carpenter.jpg',
+    workShopSmall: process.env.PUBLIC_URL + '/carpenterSmall.jpg',
     birdHouse: process.env.PUBLIC_URL + '/birdHouse.jpg',
     chalkBoard: process.env.PUBLIC_URL + '/Chalk.png',
     standardFlag: process.env.PUBLIC_URL + '/Flags/RedWhite&Blue/FlagCutOut.png',
+    standardFlagSmall: process.env.PUBLIC_URL + '/Flags/RedWhite&Blue/FlagCutOutSmall.png',
     oldBoards: process.env.PUBLIC_URL + '/Flags/old-boards.jpg',
     redLine: process.env.PUBLIC_URL + '/Flags/RedLine/FireFlagCutOut.jpg',
     contactBanner: process.env.PUBLIC_URL + '/carp-banner.jpg',
     debris: process.env.PUBLIC_URL + '/debris.jpg',
+    debrisSmall: process.env.PUBLIC_URL + '/debrisSmall.jpg',
   }
 }
