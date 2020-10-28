69270dc94bef72cdde711f0925649ca7963a5b04
GMorse19 <malorse@comcast.net>
2020-10-28 00:21:19 -0400

    Added links

---
+++ src/Pages/ProductDescription/ProductDescription.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
+
+import './ProductDescription.scss'
+
+import { itemGallery } from '../../helpers/images/Items/itemGallery'
+
+class ProductDescription extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      src: itemGallery.find(x => x.id === props.props.match.params.id).src,
+      name: itemGallery.find(x => x.id === props.props.match.params.id).name,
+      description: itemGallery.find(x => x.id === props.props.match.params.id).description,
+      price: itemGallery.find(x => x.id === props.props.match.params.id).price,
+      gallery: itemGallery.find(x => x.id === props.props.match.params.id).gallery,
+    }
+  }
+
+  render () {
+
+    const { src, name, description, price, gallery } = this.state
+
+    const galleryJsx = gallery.map(gallery => (
+      <div key={gallery.id}>
+        {
+          <div>
+            <img width={300} src={gallery.src} alt={gallery.name} />
+            <h1>{gallery.name}</h1>
+          </div>
+        }
+      </div>
+    ))
+
+    return (
+      <div className='product-display'>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Product Description</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <img width={300} src={src} alt={name}/>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center description-row">
+            <Col lg={10}>
+              <div className='product-description'>
+                <h1>{name}</h1>
+                <span>{description}</span>
+                <p>Price: {price}</p>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+          <Col>{galleryJsx}</Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default ProductDescription
