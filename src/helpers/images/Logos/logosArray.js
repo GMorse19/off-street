d3b11a3001d572f4c8ee851a78a7ecd4338ccef6
GMorse19 <malorse@comcast.net>
2020-11-19 17:04:44 -0500

    Added new cropped photos

--- src/helpers/images/Logos/logosArray.js
+++ src/helpers/images/Logos/logosArray.js
@@ -2,5 +2,5 @@ export const logo = [
   process.env.PUBLIC_URL + '/Logo/LogoOS.png',
   process.env.PUBLIC_URL + '/Logo/OSLogo.png',
   process.env.PUBLIC_URL + '/Logo/OS-Logo-no-back.png',
-  process.env.PUBLIC_URL + '/Logo/OS-Logo-no-title-no-back.png',
+  process.env.PUBLIC_URL + '/Logo/Logo-not-title.png',
 ]
